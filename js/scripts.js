b70e1b69cb52c909c895930c2be76d9a1515c8e6
Jersain Rojas <j.rojas_7@hotmail.com>
2022-09-03 11:14:24 -0700

    Week4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")){
+$(function () {
+    $(".carousel").carousel({ interval: 2000, pause: "false" });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+});
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("toggle");
+    });
+});
+
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("toggle")
     });
 });
